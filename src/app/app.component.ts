c85bb2ece4e2c3ee40ce8d96f3479d998e2e867e
Juan Jimenez <juanmjimenezt@gmail.com>
2023-08-24 15:49:05 -0400

    Property Biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'thetAngular';
+
+  public name: string | number = 'Miguel';
+  age:number = 33;
+
+  verdadero: boolean = true;
+  buttonLabel: string = 'Presiona este boton';
+
+  person = {
+    name: 'Miguel',
+    age: 25, 
+    photo: 'https://fastly.picsum.photos/id/866/300/300.jpg?hmac=9qmLpcaT9TgKd6PD37aZJZ_7QvgrVFMcvI3JQKWVUIQ'
+  }
+
+  toggleButton() { 
+    this.verdadero = !this.verdadero;
+  }
+
 }
