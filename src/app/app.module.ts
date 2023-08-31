d99714ea6e638ad08dab81d270f5b2afbc561fb4
Juan Jimenez <juanmjimenezt@gmail.com>
2023-08-31 14:41:13 -0400

    Starting Img Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ImgComponent } from './components/img/img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent
   ],
   imports: [
     FormsModule,
